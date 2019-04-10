e7802aa204b62fccd431f20104c9a092c662dbf1
Obradordigital <obradordigital@gmail.com>
2019-04-10 13:31:01 +0200

    Import module dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import * as fromMessage from './message/message.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { MessageEffects } from './message/message.effects';
 
 @NgModule({
   declarations: [
@@ -11,7 +15,12 @@ import * as fromMessage from './message/message.reducer';
   ],
   imports: [
     BrowserModule,
-    StoreModule.forFeature('message', fromMessage.reducer)
+    FormsModule,
+    StoreModule.forRoot({}),
+    StoreModule.forFeature('message', fromMessage.reducer),
+    StoreDevtoolsModule.instrument({maxAge: 25}),
+    EffectsModule.forRoot([]),
+    EffectsModule.forFeature([MessageEffects]),
   ],
   providers: [],
   bootstrap: [AppComponent]
